a804d0922c2e85a33b9c5c6fb568cfc51c04c961
wanJunHao <1007300061@qq.com>
2018-04-04 16:12:45 +0800

    test01

--- hm_data/settings.py
+++ hm_data/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'hm_data.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['appfornt'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,4 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "appfornt/static"),
+]
