96f9c41153916ed322c31a1e6906213ad441c996
郭晓敏 <gxm199272@163.com>
2018-04-17 16:07:37 +0800

    刘悦提交

---
+++ appfornt/src/components/outpatienttMap.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="mapShow" v-show="outpatienttMapShow">
+		This is mapArea.
+	</div>
+</template>
+<script type="text/javascript">
+	import outpatienttMap from "../assets/js/outpatienttMap"
+	export default{
+		...outpatienttMap
+	}
+</script>
